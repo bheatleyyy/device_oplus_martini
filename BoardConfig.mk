4a9f78fe1643c59bc63b0c4c0fec0997664ef3fc
pjgowtham <pjgowtham@gmail.com>
2022-12-10 00:40:20 +0530

    martini: Build HWC from source
    Change-Id: Iec4d65f411cafac7cdbad22ef9750f522ec6e859

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ include device/oplus/sm8350-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oplus/martini
 
 # Display
+SOONG_CONFIG_qtidisplay_udfps := true
 TARGET_SCREEN_DENSITY := 450
 
 # Partitions
