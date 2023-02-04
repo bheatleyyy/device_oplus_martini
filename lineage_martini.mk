yusufnoor786/devvice_oplus_martini

martini: Update to Android 13 (MT2111_11_F.13)
Change-Id: Ia70320d974f8ce42ee57aaeea27e1c060b8bea01

lineage_martini.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := OP5155L1
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9RT-user 12 RKQ1.211119.001 R.202209141602 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus9RT-user 13 TP1A.220624.014 R.202208300852 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9RT/OP5155L1:12/RKQ1.211119.001/R.202209141602:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9RT/OP5155L1:13/TP1A.220624.014/R.202208300852:user/release-keys

bheatleyyy/device_oplus_martini

lineage_martini.mk
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := OP5155L1
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="martini-user 12 RKQ1.211119.001 R.202209141602 release-keys" \
+    PRIVATE_BUILD_DESC="martini-user 13 TP1A.220624.014 R.202208300852 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/martini/OP5155L1:12/RKQ1.211119.001/R.202209141602:user/release-keys
+BUILD_FINGERPRINT := OnePlus/martini/OP5155L1:13/TP1A.220624.014/R.202208300852:user/release-keys